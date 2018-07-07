dfb1e08b61e2de80b3bc8aa0234560b9f531d7a6
o_s <huipo22@163.com>
2018-07-07 18:42:54 +0800

    react

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -10,17 +10,40 @@ class App extends Component {
       comments: []
     }
   }
+  componentWillMount(){
+    this._loadComments()
+  }
+  _loadComments(){
+    let comments=localStorage.getItem('comments')
+    if(comments){
+      comments=JSON.parse(comments)
+      this.setState({comments})
+    }
+  }
+  _saveComments(comments){
+    localStorage.setItem('comments',JSON.stringify(comments))
+  }
   handleSubmitComment(comment) {
-    this.state.comments.push(comment)
-    this.setState({
-      comments: this.state.comments
-    })
+    if(!comment)return
+    if(!comment.username) return alert('请输入用户名')
+    if(!comment.content) return alert('请输入评论内容')
+    const comments=this.state.comments
+    comments.push(comment)
+    this.setState({comments})
+    this._saveComments(comments)
+  }
+  handleDeleteComment(index){
+    console.log(index)
+    const comments=this.state.comments
+    comments.splice(index,1)
+    this.setState({comments})
+    this._saveComments(comments)
   }
   render() {
     return (
       <div className="App">
         <CommentInput onSubmit={this.handleSubmitComment.bind(this)} />
-        <CommentList comments={this.state.comments} />
+        <CommentList comments={this.state.comments} onDeleteComment={this.handleDeleteComment.bind(this)} />
       </div>
     );
   }
--- react-demo/src/CommentList.js
+++ react-demo/src/CommentList.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react'
 import Comment from './Comment'
 
 class CommentList extends Component {
-   
+    static defaultProps = {
+        comments: []
+    }
+    handleDeleteComment(index) {
+        if (this.props.onDeleteComment) {
+            this.props.onDeleteComment(index)
+        }
+    }
     render() {
         return (
             <div>
-                {this.props.comments.map((comment,index)=><Comment comment={comment} key={index}/>)}
+                {this.props.comments.map((comment, index) => <Comment comment={comment} key={index} index={index} onDeleteComment={this.handleDeleteComment.bind(this)} />)}
             </div>
         )
     }
